431851124a5a23c5076f66ac52319935a22b2caa
sangjinsu <climbplant39@kakao.com>
2024-02-10 21:37:38 +0900

    if-let 흐름제어

--- enum-pattern-matching/src/main.rs
+++ enum-pattern-matching/src/main.rs
@@ -99,4 +99,28 @@ fn main() {
         7 => remove_fancy_hat(),
         _ => (),
     }
+
+
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {}", max),
+        _ => (),
+    }
+
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+
+    // let mut count = 0;
+    // match coin {
+    //     Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+    //     _ => count += 1,
+    // }
+
+    // let mut count = 0;
+    // if let Coin::Quarter(state) = coin {
+    //     println!("State quarter from {:?}!", state);
+    // } else {
+    //     count += 1;
+    // }
 }
