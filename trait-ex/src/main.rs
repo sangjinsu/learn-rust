1a99fed8906d7dae4ff8230a0a5218f9cff02662
sangjinsu <climbplant39@kakao.com>
2024-02-14 22:54:32 +0900

    ✨ 트레잇

---
+++ trait-ex/src/main.rs
@@ -0,0 +1,70 @@
+pub trait Summarizable {
+    fn summary(&self) -> String;
+    fn author_summary(&self) -> String;
+
+}
+
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summarizable for NewsArticle {
+    fn summary(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+
+    fn author_summary(&self) -> String {
+        todo!()
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summarizable for Tweet {
+    fn summary(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+
+    fn author_summary(&self) -> String {
+        todo!()
+    }
+}
+
+struct WeatherForecast {
+    high_temp: f64,
+    low_temp: f64,
+    chance_of_precipitation: f64,
+}
+
+impl Summarizable for WeatherForecast {
+    fn summary(&self) -> String {
+        format!("The high will be {}, and the low will be {}. The chance of
+        precipitation is {}%.", self.high_temp, self.low_temp,
+                self.chance_of_precipitation)
+    }
+
+    fn author_summary(&self) -> String {
+        todo!()
+    }
+}
+
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summary());
+}
